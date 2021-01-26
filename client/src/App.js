75506e74f98b7946b2e36a11bd4b561227524d76
Prakash <prkjha25@gmail.com>
2021-01-26 15:28:24 +0530

    nav and user added

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { UserContext } from './UserContext'
+import Chat from './components/chat/Chat'
+import Home from './components/home/Home'
+import Navbar from './components/layout/Navbar'
+import React, { useEffect, useState } from 'react'
+
+function App() {
+  const [user, setUser] = useState(null)
+  return (
+    <Router>
+      <div className='App'>
+        <UserContext.Provider value={{user,setUser}}>
+          <Navbar/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/chat" component={Chat} />
+        </Switch>
+        </UserContext.Provider>
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ client/src/components/home/Home.js
@@ -0,0 +1,37 @@
+import React,{useContext} from 'react'
+import {UserContext} from '../../UserContext'
+import {Link} from 'react-router-dom'
+
+const Home = () => {
+  const {user,setUser} = useContext(UserContext)
+  const setAsJohn = () => {
+    const john = {
+      name:'John',
+      email:'john@email.com',
+      password:'123',
+      id:'123'
+    }
+    setUser(john)
+  }
+  const setAsTom = () => {
+    const tom = {
+      name:'Tom',
+      email:'tom@email.com',
+      password:'123',
+      id:'456'
+    }
+    setUser(tom)
+  }
+  return (
+    <div>
+      <h1>Home {JSON.stringify(user)}</h1>
+      <button onClick={setAsJohn}> set as John</button>
+      <button onClick={setAsTom}> set as Tom</button>
+      <Link to={'/chat'}>
+        <button>go to chat</button>
+      </Link>
+    </div>
+  )
+}
+
+export default Home
