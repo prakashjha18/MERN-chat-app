9e9c914153619a53ffb67ddeeddf0377c552bbee
prakash <prkjha25@gmail.com>
2021-01-27 00:55:00 +0530

    chat rom socket connection

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,10 +1,28 @@
-import React, { useContext } from 'react'
+import React, { useContext,useState,useEffect } from 'react'
 import { UserContext } from '../../UserContext'
 import { Link } from 'react-router-dom'
 import RoomList from './RoomList'
-
+import io from 'socket.io-client'
+let socket;
 const Home = () => {
+  const ENDPT = 'localhost:5000'
+
+  useEffect(()=> {
+    socket = io(ENDPT)
+    return () => {
+      socket.emit('disconnect');
+      socket.off();
+    }
+  },[ENDPT])
+
   const { user, setUser } = useContext(UserContext)
+  const [room,setRoom]=useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit('create-room',room)
+    console.log(room)
+    setRoom('')
+  }
   const rooms = [
     {
       name: 'room1',
@@ -46,13 +64,15 @@ const Home = () => {
               </span>
 
               <div className='row'>
-                <form>
+                <form onSubmit={handleSubmit}>
                   <div className='row'>
                     <div className='input-field col s12'>
                       <input
                         placeholder='Enter a room name'
                         id='room'
                         type='text'
+                        value={room}
+                        onChange={e => setRoom(e.target.value)}
                         className='validate'
                       />
                       <label htmlFor='room'>room</label>
