466e7384f441ab4d31ea95d7c0b07a48237af20c
prakash <prkjha25@gmail.com>
2021-01-27 01:22:32 +0530

    join chat room for users

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,13 +1,24 @@
-import React, { useContext } from 'react'
+import React, { useContext,useEffect } from 'react'
 import { UserContext } from '../../UserContext'
 import { Link, useParams } from 'react-router-dom'
+import io from 'socket.io-client'
+let socket
 
 const Chat = () => {
+  const ENDPT = 'localhost:5000'
+
   const { user, setUser } = useContext(UserContext)
   let { room_id, room_name } = useParams()
+
+  useEffect(() => {
+    socket = io(ENDPT)
+    socket.emit('join', { name: user.name, room_id, user_id: user.id })
+  }, [])
   return (
     <div>
-      <div>{room_id} {room_name}</div>
+      <div>
+        {room_id} {room_name}
+      </div>
       <h1>Chat {JSON.stringify(user)}</h1>
       <Link to={'/'}>
         <button>go to home</button>
--- server/index.js
+++ server/index.js
@@ -1,16 +1,30 @@
-const app = require('express')();
-const http = require('http').createServer(app);
+const app = require('express')()
+const http = require('http').createServer(app)
 const socketio = require('socket.io')
-const io = socketio(http);
-const PORT = process.env.PORT || 5000 
+const io = socketio(http)
+const { addUser } = require('./helper')
+const PORT = process.env.PORT || 5000
 
 io.on('connection', (socket) => {
-  console.log(socket.id);
-  socket.on('create-room', name => {
-      console.log('then room name received is', name)
+  console.log(socket.id)
+  socket.on('create-room', (name) => {
+    console.log('then room name received is', name)
   })
-});
+  socket.on('join', ({ name, room_id, user_id }) => {
+    const { error, user } = addUser({
+      socket_id: socket.id,
+      name,
+      room_id,
+      user_id,
+    })
+    if (error) {
+      console.log('join error', error)
+    } else {
+        console.log('join user',user)
+    }
+  })
+})
 
 http.listen(PORT, () => {
-  console.log(`listening on :${PORT}`);
-});
+  console.log(`listening on :${PORT}`)
+})
