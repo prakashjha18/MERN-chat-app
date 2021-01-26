b0fe39768b64e4eb2d15614172d3668d877d8a1b
prakash <prkjha25@gmail.com>
2021-01-26 15:40:08 +0530

    create a form

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,32 +1,65 @@
-import React,{useContext} from 'react'
-import {UserContext} from '../../UserContext'
-import {Link} from 'react-router-dom'
+import React, { useContext } from 'react'
+import { UserContext } from '../../UserContext'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
-  const {user,setUser} = useContext(UserContext)
+  const { user, setUser } = useContext(UserContext)
   const setAsJohn = () => {
     const john = {
-      name:'John',
-      email:'john@email.com',
-      password:'123',
-      id:'123'
+      name: 'John',
+      email: 'john@email.com',
+      password: '123',
+      id: '123',
     }
     setUser(john)
   }
   const setAsTom = () => {
     const tom = {
-      name:'Tom',
-      email:'tom@email.com',
-      password:'123',
-      id:'456'
+      name: 'Tom',
+      email: 'tom@email.com',
+      password: '123',
+      id: '456',
     }
     setUser(tom)
   }
   return (
     <div>
-      <h1>Home {JSON.stringify(user)}</h1>
-      <button onClick={setAsJohn}> set as John</button>
-      <button onClick={setAsTom}> set as Tom</button>
+      <div className='row'>
+        <div className='col s12 m6'>
+          <div className='card blue-grey darken-1'>
+            <div className='card-content white-text'>
+              <span className='card-title'>
+                welcome {user ? user.name : ''}
+              </span>
+
+              <div className='row'>
+                <form   >
+                  <div className='row'>
+                    <div className='input-field col s12'>
+                      <input
+                        placeholder='Enter a room name'
+                        id='room'
+                        type='text'
+                        className='validate'
+                      />
+                      <label htmlFor='room'>room</label>
+                    </div>
+                  </div>
+                  <button className="btn">Create room</button>
+                </form>
+              </div>
+            </div>
+            <div className='card-action'>
+              <a href='#' onClick={setAsJohn}>
+                set as John
+              </a>
+              <a href='#' onClick={setAsTom}>
+                set as Tom
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
       <Link to={'/chat'}>
         <button>go to chat</button>
       </Link>
