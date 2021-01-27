3e947be6f7a942a99f97f9783c965ddb7572674a
prakash <prkjha25@gmail.com>
2021-01-27 14:40:28 +0530

    send and receive message

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,4 +1,4 @@
-import React, { useContext,useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../../UserContext'
 import { Link, useParams } from 'react-router-dom'
 import io from 'socket.io-client'
@@ -9,20 +9,43 @@ const Chat = () => {
 
   const { user, setUser } = useContext(UserContext)
   let { room_id, room_name } = useParams()
+  const [message, SetMessage] = useState('')
+  const [messages,setMessages] = useState([])
 
   useEffect(() => {
     socket = io(ENDPT)
     socket.emit('join', { name: user.name, room_id, user_id: user.id })
   }, [])
+  useEffect(() => {
+    socket.on('message',message=>{
+      setMessages([...messages,message])
+    })
+  },[messages])
+  const sendMessage = (event) => {
+    event.preventDefault()
+    if (message) {
+      console.log(message)
+      socket.emit('sendMessage', message, room_id, () => SetMessage(''))
+    }
+  }
   return (
     <div>
       <div>
         {room_id} {room_name}
       </div>
       <h1>Chat {JSON.stringify(user)}</h1>
-      <Link to={'/'}>
-        <button>go to home</button>
-      </Link>
+      <pre>{JSON.stringify(messages,null,'\t')}</pre>
+      <form action='' onSubmit={sendMessage}>
+        <input
+          type='text'
+          value={message}
+          onChange={(event) => SetMessage(event.target.value)}
+          onKeyPress={(event) =>
+            event.key === 'Enter' ? sendMessage(event) : null
+          }
+        />
+        <button>Send Message</button>
+      </form>
     </div>
   )
 }
